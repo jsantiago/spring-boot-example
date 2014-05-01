8945f735196a3b1617bea4cd40a9f560fe84d35b
J. Santiago <j2santiago@gmail.com>
2014-05-01 14:42:49 -0500

    add /reverse and /foo

--- src/main/java/com/example/application/web/ApplicationController.java
+++ src/main/java/com/example/application/web/ApplicationController.java
@@ -1,10 +1,13 @@
 package com.example.application.web;
 
+import com.example.application.domain.Message;
 import com.example.application.service.ApplicationService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -16,8 +19,33 @@ public class ApplicationController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
-    String home() {
+    public String home() {
         return applicationService.sayHello();
+    }
+
+    @RequestMapping(value = "/reverse", method = RequestMethod.POST)
+    @ResponseBody
+    public Message reverse(@Validated @RequestBody Message message) {
+        Message response = new Message();
+
+        if (message != null) {
+            if (StringUtils.isNotEmpty(message.getTitle())) {
+                response.setTitle(message.getTitle());
+            }
+
+            if (StringUtils.isNotEmpty(message.getValue())) {
+                String reversedString = StringUtils.reverse(message.getValue());
+                response.setValue(reversedString);
+            }
+        }
+
+        return response;
+    }
+
+    @RequestMapping("/foo")
+    @ResponseBody
+    public String foo() {
+        throw new IllegalArgumentException("Server error");
     }
 
     public static void main(String[] args) throws Exception {
